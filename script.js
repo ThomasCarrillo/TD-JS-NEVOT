cd6c87cdcb2bb121e843dd7f78b3927a5aaa9fb8
Thomas CARRILLO <thomas.carrillo84@gmail.com>
2019-02-13 14:46:15 +0100

    [FEAT] Ajout de bouton et de la section géolocalisation

--- script.js
+++ script.js
@@ -36,24 +36,70 @@
     }
 };
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
 $(document).ready(function(){
   var scrollLink  = $('.scroll');
   scrollLink.click(function(e){
     e.preventDefault();
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top
-    }, 2000);
+    }, 1000);
   });
 });
 
-$(document).ready(function() {
-  var link = document.getElementsByClassName(".nav-item");
-  $(".nav-item").on("click", function() {
-    if(link.className == "active"){
-      $("nav ul li").removeClass("active");
+$(document).ready(function(){
+  var alert = document.getElementById("submit");
+  var target = document.getElementsByClassName("needs-validation")
+  alert.addEventListener("click", function(){
+    if(target.className === "was-validated"){
+      swal("Succès !", "Le formulaire à bien été envoyé !", "success");
     }
     else {
-      $("nav ul li").toggleClass("active");
+      swal("Erreur !", "Veuillez vérifier les informations rentrées !", "error");
     }
   });
 });
+
+$(document).ready(function(){
+  $("#validationServerUsername").change(function(){
+    if(!$("#validationServerUsername").val().match(/^[a-zA-Z0-9]{6,24}$/)){
+      $("#usernameFeedback").css("display", "block");
+    }
+    else {
+      $("#usernameFeedback").css("display", "none");
+    }
+  });
+});
+
+function showPosition(position) {
+  var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&sensor=false&key=YOUR_:KEY";
+
+  document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
+
+function initMap() {
+  var mapProp= {
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
